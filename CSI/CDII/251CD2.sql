5a112df7563a27e819bd252ecad5d7e2a97253d9
andreneves <andr@andr.com.br>
2025-03-11 19:51:19 -0300

    up

--- CSI/CDII/251CD2.sql
+++ CSI/CDII/251CD2.sql
@@ -30,3 +30,44 @@ from alunos
 where idade = 28;
 
 
+------------------
+-- RENAME TABLE --
+------------------
+
+-- Explicação: Renomeia uma tabela do banco de dados selecionado.
+-- Sintaxe: 
+
+-- RENAME TABLE NomeDaTabela; 
+
+RENAME TABLE clientes TO Meus_Clientes;
+
+
+
+----------------
+-- DROP TABLE --
+----------------
+
+-- Explicação: Destrói uma tabela do banco de dados selecionado.
+-- Sintaxe: 
+
+-- DROP TABLE NomeDaTabela; 
+
+-- Exemplo: 
+
+DROP TABLE clientes; -- deleta a tabela
+
+
+--------------------
+-- TRUNCATE TABLE --
+--------------------
+
+-- Explicação: Limpa todos os dados, mas preserva a estrutura da tabela. Também volta o auto_increment para o status inicial.
+-- Sintaxe: 
+
+-- TRUNCATE TABLE NomeDaTabela; 
+
+
+-- Exemplo: 
+
+TRUNCATE TABLE clientes;
+
